e004fb5deaa014af671fc8ec61b9a7109798bcf8
ahmed <ahmed.elsherif010@gmail.com>
2022-09-23 18:46:17 +0200

    added Widgets Directory

---
+++ lib/presentation/widgets/gradiant_background.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../styles/colors.dart';
+
+class GradiantBackground extends StatelessWidget {
+  const GradiantBackground({
+    Key? key,
+    this.gradiantColor = const [AppColor.white, AppColor.primaryColor],
+    this.child,
+    this.decorationImage,
+    this.stops = const [0.5, 0.8],
+    this.begin = Alignment.topCenter,
+    this.end = Alignment.bottomCenter,
+  }) : super(key: key);
+
+  final List<Color> gradiantColor;
+  final Widget? child;
+  final DecorationImage? decorationImage;
+  final List<double> stops;
+  final Alignment begin;
+  final Alignment end;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      child: child,
+      decoration: BoxDecoration(
+        image: decorationImage,
+        gradient: LinearGradient(
+          //begin: const FractionalOffset(0.0, 0.0),
+          //   end: const FractionalOffset(1.0, 0.0),
+          begin: begin,
+          end: end,
+          stops: stops,
+          colors: gradiantColor,
+        ),
+      ),
+    );
+  }
+}
