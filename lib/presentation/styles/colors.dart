0aa6129b364e57fcbc87e1a0ba524fa88feb9baa
ahmed <ahmed.elsherif010@gmail.com>
2022-09-23 18:35:00 +0200

    added Colors Palette

--- lib/presentation/styles/colors.dart
+++ lib/presentation/styles/colors.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
+
+  /// Most useful Colors
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color grey = Color(0xFF9E9E9E);
-  static const Color green = Color(0xFF42CCA2);
   static const Color darkGrey = Color(0xFF2D2D2D);
   static const Color backGroundColor = Colors.grey;
   static const Color transparent = Colors.transparent;
+
+  /// Color Palette
+  static const Color mintGreen = Color(0xFFa1f2ac);
+  static const Color lightGreen = Color(0xFFaaffb5);
+  static const Color primaryColor = Color(0xFF80bf88);
+  static const Color murdochGreen = Color(0xFF5d8c64);
+  static const Color deepMossGreen = Color(0xFF3c5a40);
 }
