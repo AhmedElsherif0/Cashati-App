5ea6517b6e27235f45f345475988cf33811c29a6
ahmed <ahmed.elsherif010@gmail.com>
2022-10-02 06:19:26 +0200

    TabBarView Ready

---
+++ lib/presentation/widgets/tab_view_item_decoration.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '../styles/colors.dart';
+
+class TabBarItem extends StatelessWidget {
+  const TabBarItem({Key? key, required this.text}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.sp),
+        border: Border.all(width: 1.sp, color: AppColor.primaryColor),
+      ),
+      child: Align(alignment: Alignment.center, child: Text(text)),
+    );
+  }
+}
