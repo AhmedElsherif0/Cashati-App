95900b85db69abedd06a37937fdb7da03a3b8ad2
ahmed <ahmed.elsherif010@gmail.com>
2022-10-02 03:16:02 +0200

    added AppStyle class and updated Themes

--- lib/presentation/styles/styles.dart
+++ lib/presentation/styles/styles.dart
@@ -1,18 +1,65 @@
-// import 'package:flutter/material.dart';
-// import 'package:sizer/sizer.dart';
-// import 'colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sizer/sizer.dart';
+import 'colors.dart';
 
-// TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
-//   return TextStyle(fontSize: fontSize, color: color, fontWeight: fontWeight);
-// }
+class AppStyle {
+  TextStyle headline1Theme() => const TextStyle(
+      fontWeight: FontWeight.w600, color: AppColor.black, fontSize: 32);
 
-// TextStyle getRegularStyle({double fontSize = 14, required Color color}) {
-//   return _getTextStyle(fontSize, FontWeight.normal, color);
-// }
+  TextStyle headline2Theme() => const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      color: AppColor.pineGreen,
+      letterSpacing: -0.3);
 
-// TextStyle getBoldStyle({double fontSize = 16, required Color color}) {
-//   return _getTextStyle(fontSize, FontWeight.bold, color);
-// }
+  TextStyle headline3Theme() => TextStyle(
+      fontSize: 16.sp, fontWeight: FontWeight.bold, color: AppColor.pineGreen);
+
+  TextStyle headline4Theme() => TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.bold,
+      color: AppColor.primaryColor,
+      letterSpacing: -0.3);
+
+  TextStyle headline5Theme() => TextStyle(
+      fontSize: 16.sp, fontWeight: FontWeight.w500, color: AppColor.pineGreen);
+
+  TextStyle headline6Theme() => TextStyle(
+      fontSize: 12.4.sp,
+      fontWeight: FontWeight.w600,
+      color: AppColor.primaryColor);
+
+  TextStyle bodyText1Theme() => TextStyle(
+      fontSize: 12.4.sp,
+      fontWeight: FontWeight.w500,
+      color: AppColor.white,
+      letterSpacing: -0.3);
+
+  TextStyle bodyText2Theme() => TextStyle(
+      fontSize: 12.sp,
+      fontWeight: FontWeight.bold,
+      color: AppColor.primaryColor,
+      letterSpacing: -0.3);
+
+  TextStyle subtitle1Theme() => TextStyle(
+      fontSize: 12.4.sp,
+      fontWeight: FontWeight.w300,
+      color: AppColor.pineGreen);
+
+  TextStyle subtitle2Theme() => const TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w400, color: AppColor.grey);
+
+  TextStyle appBarTitleTextStyle() => TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.bold,
+      color: AppColor.primaryColor);
+
+  SystemUiOverlayStyle systemUiStatusBarStyle() => const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark);
+}
 
 // BoxDecoration defBoxDecoration = BoxDecoration(
 //     color: AppColor.white,
