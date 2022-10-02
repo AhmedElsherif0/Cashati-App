f13385bb8e33e72e1bd442575d874b92a4206850
‪Mostafa Omar‬‏ <51782392+moustafadev@users.noreply.github.com>
2022-10-02 14:55:22 +0300

    add home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:sizer/sizer.dart';
+import 'package:temp/presentation/styles/colors.dart';
 
 import '../../../business_logic/global_cubit/global_cubit.dart';
 import '../../../constants/language_manager.dart';
@@ -15,14 +17,98 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 90),
+      padding: const EdgeInsets.only(top: 90),
       child: Column(
         children: [
           CustomAppBar(
             title: 'Home',
             onTapBack: () {},
             onTanNotification: () {},
-          )
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, top: 80),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                          bottom: 5, // Space between underline and text
+                        ),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.green,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Expenses",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                          bottom: 5, // Space between underline and text
+                        ),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.green,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Income",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 7.h,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: 55.h,
+                    decoration: const BoxDecoration(
+                      color: AppColor.primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
