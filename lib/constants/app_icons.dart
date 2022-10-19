9e69b515048769bf01bf63019ca494da46f19f1c
ahmed <ahmed.elsherif010@gmail.com>
2022-10-19 02:17:30 +0200

    constants edited

--- lib/constants/app_icons.dart
+++ lib/constants/app_icons.dart
@@ -1,7 +1,27 @@
-class AppIcons{
-  static const String reminderIcon='assets/icons/reminderIcon.svg';
-  static const String notificationSettingIcon='assets/icons/notificationIconAlert.svg';
-  static const String editCategoryIcon='assets/icons/editCategoryIcon.svg';
-  static const String languageSettingIcon='assets/icons/languageSettingIcon.svg';
-  static const String currencySettingsIcon='assets/icons/currencySettingsIcon.svg';
+class AppIcons {
+  static const String reminder = 'assets/icons/reminderIcon.svg';
+  static const String notificationSetting =
+      'assets/icons/notificationIconAlert.svg';
+  static const String editCategoryIcon = 'assets/icons/editCategoryIcon.svg';
+  static const String languageSetting =
+      'assets/icons/languageSettingIcon.svg';
+  static const String currencySettings =
+      'assets/icons/currencySettingsIcon.svg';
+
+  /// Nav Bar Item Icons.
+  static const String navDollarSign = 'assets/icons/dollar.svg';
+  static const String navExpenseWallet = 'assets/icons/wallet.svg';
+  static const String navIncomeWallet = 'assets/icons/wallet.svg';
+  static const String navGlowChart = 'assets/icons/glow_chart.svg';
+  static const String navSettings = 'assets/icons/setting.svg';
+  static const String noDataCate = 'assets/images/No data-cuate 1.png';
+
+  /// Confirm Paying Cards Icons.
+   static const String categories =  'assets/icons/confirm_paying/categories_icon.svg';
+   static const String poundSterlingSign =  'assets/icons/confirm_paying/pound_sign_icon.svg';
+   static const String editIcon =  'assets/icons/confirm_paying/edit-Icons.svg';
+   static const String calender = 'assets/icons/confirm_paying/calender_icon.svg';
+   static const String exclamationMark = 'assets/icons/confirm_paying/exclamation_mark.svg';
+   static const String delete = 'assets/icons/confirm_paying/delete_Icons.svg';
+
 }
